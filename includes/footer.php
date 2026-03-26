13467836edb1b1fc6d2e99551be4d73214fd981d
Paulo Henrique Pereira <phwtsp@gmail.com>
2026-03-26 11:19:41 -0300

    style: switch logo to svg and remove redundant image-rendering properties

--- includes/footer.php
+++ includes/footer.php
@@ -11,7 +11,7 @@ if (isset($page_scripts) && is_array($page_scripts)) {
 
 $currentPage = isset($current_page) && is_string($current_page) ? $current_page : '';
 $whiteFooterLogoPages = ['sobre', 'pacaembu', 'novasuissa'];
-$footerLogo = in_array($currentPage, $whiteFooterLogoPages, true) ? 'assets/images/Logo_Menu.png' : 'assets/images/01_MZB_logo01.webp';
+$footerLogo = in_array($currentPage, $whiteFooterLogoPages, true) ? 'assets/images/Logo_Menu.png' : 'assets/images/MZB.svg';
 ?>
 <!-- Footer -->
 <footer class="<?= htmlspecialchars($footerClass, ENT_QUOTES, 'UTF-8') ?>">
